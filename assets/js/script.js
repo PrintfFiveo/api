eb44f71cb1018058c4c6053988a017c62fbf70d4
Vitor Tavares <vitor.tavares.leite@gmail.com>
2024-05-19 15:39:03 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ const genreDisplay = document.getElementById('genreDisplay');
 
 generateButton.addEventListener('click', async () => {
   try {
-    const response = await fetch('https://binaryjazz.us/wp-json/genrenator/v1/'); 
+    const response = await fetch('https://binaryjazz.us/wp-json/genrenator/v1/genre/5/'); 
     const data = await response.json();
     console.log(data);
-    genreDisplay.textContent = data.genre; 
+    genreDisplay.textContent = data; 
 
   } catch (error) {
     console.error('Erro no fetch:', error);
